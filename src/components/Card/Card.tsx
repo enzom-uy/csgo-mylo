0a9d45135b9c4238923b1866298fc5b9cddea804
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 14:37:22 -0300

    Make Card component more reusable with Props

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface Props {
+    altText: string
+    title: string
+    img: string
+}
+
+const Card: React.FC<Props> = ({ altText, title, img }) => {
+    return (
+        <div className="w-full text-center relative overflow-hidden drop-shadow-heading cursor-pointer">
+            <img src={img} className="object-cover h-48 w-96" alt={altText} />
+            <h1 className="text-white text-3xl font-semibold absolute top-[50%] left-[50%] translate-x-[-58%] translate-y-[-75%]">
+                {title}
+            </h1>
+        </div>
+    )
+}
+
+export default Card
