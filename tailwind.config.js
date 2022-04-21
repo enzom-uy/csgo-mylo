b919732291f5625a04e075c797d7b0794189c124
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 00:32:32 -0300

    config: customizing tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'navbar-logo': "url('/src/assets/mylo_navbarpng.png')",
+                'card-dust2': "url('/src/assets/card_dust2.png')",
+                'card-inferno': "url('/src/assets/card_inferno.png')",
+                'card-mirage': "url('/src/assets/card_mirage.png')",
+                'card-nuke': "url('/src/assets/card_nuke.png')",
+                'card-overpass': "url('/src/assets/card_overpass.png')"
+            },
+            colors: {
+                'background-start': '#161925',
+                'background-end': '#191C2E',
+                'mobile-text': '#406E8E'
+            }
+        }
+    },
+    plugins: []
+}
