99211f47de457ca8f41b44942ffb607d53884401
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 02:29:49 -0300

    wip: added NavLink inside li

--- src/components/Navbar/MobileMenuItems.tsx
+++ src/components/Navbar/MobileMenuItems.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../providers/ThemeContext'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const MobileMenuItems: React.FC = () => {
     const currentLocation = useLocation().pathname
-    console.log(currentLocation)
     return (
         <ul className="fixed right-0 left-0 top-[10.5%] bg-background-start w-full shadow-custom">
             <li
@@ -11,21 +11,21 @@ const MobileMenuItems: React.FC = () => {
                     currentLocation === '/' ? 'bg-red-900' : ''
                 }`}
             >
-                Inicio
+                <NavLink to="/">Inicio</NavLink>
             </li>
             <li
                 className={`text-center ${
                     currentLocation === '/mapas' ? 'bg-red-900' : ''
                 }`}
             >
-                Mapas
+                <NavLink to="/mapas">Mapas</NavLink>
             </li>
             <li
                 className={`text-center ${
                     currentLocation === '/agregar' ? 'bg-red-900' : ''
                 }`}
             >
-                Agregar nade
+                <NavLink to="/agregar">Agregar nade</NavLink>
             </li>
         </ul>
     )
