7c63da1ce6debd7d00ec55b5c8b3f4ced25c5c2a
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 16:56:01 -0300

    Finish Mapas page initial state

--- src/pages/Mapas.tsx
+++ src/pages/Mapas.tsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import Card from '../components/Card/Card'
+import miragecard from '/src/assets/card_mirage.png'
+import infernocard from '/src/assets/card_inferno.png'
+import overpasscard from '/src/assets/card_overpass.png'
+import dust2card from '/src/assets/card_dust2.png'
+import nukecard from '/src/assets/card_nuke.png'
+
+const maps = [
+    {
+        title: 'Mirage',
+        alt: 'Mapa de Mirage',
+        img: miragecard
+    },
+    {
+        title: 'Inferno',
+        alt: 'Mapa de Inferno',
+        img: infernocard
+    },
+    {
+        title: 'Overpass',
+        alt: 'Mapa de Overpass',
+        img: overpasscard
+    },
+    {
+        title: 'Dust 2',
+        alt: 'Mapa de Dust 2',
+        img: dust2card
+    },
+    {
+        title: 'Nuke',
+        alt: 'Mapa de Nuke',
+        img: nukecard
+    }
+]
 
 const Mapas: React.FC = () => {
-    return
+    return (
+        <div className="flex flex-wrap w-full justify-center">
+            {maps.map((map) => (
+                <Card
+                    key={map.title}
+                    title={map.title}
+                    altText={map.alt}
+                    img={map.img}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default Mapas
