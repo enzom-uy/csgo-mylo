942453d922ec336be7030f36a0f596c54c0b5ed1
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 14:38:03 -0300

    Finish Homepage initial state

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Card from '../components/Card/Card'
+import mapasimg from '/src/assets/card_mapas.png'
 
 const Homepage: React.FC = () => {
-    return <div></div>
+    return (
+        <main className="flex flex-col items-center">
+            <h1
+                role="heading"
+                className="text-mobile-text text-3xl font-semibold drop-shadow-heading mb-12"
+            >
+                MYLO NADES
+            </h1>
+            <div>
+                <Card
+                    altText="Multiple csgo maps blurred"
+                    title="Mapas"
+                    img={mapasimg}
+                />
+            </div>
+        </main>
+    )
 }
 
 export default Homepage
