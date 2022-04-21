8772fedf2c1e6f698fb548661c0bd4b50bbb50bb
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 00:35:47 -0300

    wip: conditional rendering MobileMenu

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../providers/ThemeContext'
+import MobileMenu from './MobileMenu'
+import logo from '/src/assets/mylo_navbarpng.png'
+
+const Navbar = () => {
+    const { isMobile } = useContext(ThemeContext)
+    return (
+        <nav
+            role="navigation"
+            className="flex pt-[10px] items-center justify-between"
+        >
+            <div>
+                <img src={logo} />
+            </div>
+            {isMobile ? <MobileMenu /> : ''}
+        </nav>
+    )
+}
+
+export default Navbar
