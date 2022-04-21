df2f6d4c393f7853dc3131a4b92894c4aa8dc9a8
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 15:44:34 -0300

    Navbar horizontal padding when viewport is Desktop

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../../providers/ThemeContext'
 import MobileMenu from './MobileMenu'
+import DesktopMenu from './DesktopMenu'
 import logo from '/src/assets/mylo_navbarpng.png'
 
 const Navbar: React.FC = () => {
@@ -8,12 +9,14 @@ const Navbar: React.FC = () => {
     return (
         <nav
             role="navigation"
-            className="flex pt-[10px] pb-[46px] items-center justify-between"
+            className={`flex pt-[10px] pb-[46px] ${
+                !isMobile ? 'px-5' : null
+            } items-center justify-between`}
         >
             <div>
                 <img src={logo} />
             </div>
-            {isMobile ? <MobileMenu /> : ''}
+            {isMobile ? <MobileMenu /> : <DesktopMenu />}
         </nav>
     )
 }
