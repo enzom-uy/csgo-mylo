2b5dc913fd1bf40aa518c0d5eff042c654df126d
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-21 16:54:53 -0300

    Fix style for Card component, now cards wrap

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Card: React.FC<Props> = ({ altText, title, img }) => {
     return (
-        <div className="w-full text-center relative overflow-hidden drop-shadow-heading cursor-pointer">
+        <div className="text-center relative overflow-hidden drop-shadow-heading cursor-pointer">
             <img src={img} className="object-cover h-48 w-96" alt={altText} />
             <h1 className="text-white text-3xl font-semibold absolute top-[50%] left-[50%] translate-x-[-58%] translate-y-[-75%]">
                 {title}
