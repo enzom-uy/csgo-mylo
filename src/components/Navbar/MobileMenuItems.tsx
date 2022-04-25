8a859cd2ddb0463d0d7e67326fe41fd12c632c61
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-25 16:46:33 -0300

    Fix MobilemenuItems style

--- src/components/Navbar/MobileMenuItems.tsx
+++ src/components/Navbar/MobileMenuItems.tsx
@@ -1,30 +1,43 @@
 import React from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
-const MobileMenuItems: React.FC = () => {
+interface Props {
+    toggle: () => void
+}
+
+const MobileMenuItems: React.FC<Props> = ({ toggle }) => {
     const currentLocation = useLocation().pathname
     return (
-        <ul className="fixed right-0 left-0 top-[10.5%] bg-background-start w-full shadow-custom z-10">
+        <ul className="fixed right-0 left-0 top-[8.5%] bg-background-start w-full shadow-custom">
             <li
+                onClick={toggle}
                 className={`text-center ${
-                    currentLocation === '/' ? 'bg-red-900' : ''
-                }`}
+                    currentLocation === '/' ? 'bg-active-link' : ''
+                } h-10 flex justify-center items-center cursor-pointer`}
             >
-                <NavLink to="/">Inicio</NavLink>
+                <NavLink to="/" className="text-text-color w-full">
+                    Inicio
+                </NavLink>
             </li>
             <li
+                onClick={toggle}
                 className={`text-center ${
-                    currentLocation === '/mapas' ? 'bg-red-900' : ''
-                }`}
+                    currentLocation === '/mapas' ? 'bg-active-link' : ''
+                } h-10 flex justify-center items-center cursor-pointer`}
             >
-                <NavLink to="/mapas">Mapas</NavLink>
+                <NavLink to="/mapas" className="text-text-color w-full">
+                    Mapas
+                </NavLink>
             </li>
             <li
+                onClick={toggle}
                 className={`text-center ${
-                    currentLocation === '/agregar' ? 'bg-red-900' : ''
-                }`}
+                    currentLocation === '/agregar' ? 'bg-active-link' : ''
+                } h-10 flex justify-center items-center cursor-pointer`}
             >
-                <NavLink to="/agregar">Agregar nade</NavLink>
+                <NavLink to="/agregar" className="text-text-color w-full">
+                    Agregar nade
+                </NavLink>
             </li>
         </ul>
     )
