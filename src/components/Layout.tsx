e6e937196c8d0e44f34c4f48f2ea75ff1230471c
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-25 16:46:50 -0300

    Fix Layout style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './Navbar/Navbar'
+import { ThemeContext } from '../providers/ThemeContext'
 
 interface Props {
     children: React.ReactNode
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
+    const { isMobile } = useContext(ThemeContext)
     return (
         <div className="w-full">
             <Navbar />
-            <main className="flex justify-center flex-wrap">{children}</main>
+            <main
+                className={`flex justify-center flex-wrap px-5 ${
+                    isMobile ? 'pt-[7rem]' : 'pt-[7rem]'
+                }`}
+            >
+                {children}
+            </main>
         </div>
     )
 }
