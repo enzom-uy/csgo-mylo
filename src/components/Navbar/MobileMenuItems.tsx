a98014614f2390f604b14b350c33aa5265b4d28e
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-28 20:46:24 -0300

    Fix MobileMenuItems position

--- src/components/Navbar/MobileMenuItems.tsx
+++ src/components/Navbar/MobileMenuItems.tsx
@@ -2,45 +2,45 @@ import React from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
 interface Props {
-    toggle: () => void
+  toggle: () => void
 }
 
 const MobileMenuItems: React.FC<Props> = ({ toggle }) => {
-    const currentLocation = useLocation().pathname
-    return (
-        <ul className="fixed right-0 left-0 top-[8.5%] bg-background-start w-full shadow-custom">
-            <li
-                onClick={toggle}
-                className={`text-center ${
-                    currentLocation === '/' ? 'bg-primary-darker' : ''
-                } h-10 flex justify-center items-center cursor-pointer`}
-            >
-                <NavLink to="/" className="text-text-color w-full">
-                    Inicio
-                </NavLink>
-            </li>
-            <li
-                onClick={toggle}
-                className={`text-center ${
-                    currentLocation === '/mapas' ? 'bg-primary-darker' : ''
-                } h-10 flex justify-center items-center cursor-pointer`}
-            >
-                <NavLink to="/mapas" className="text-text-color w-full">
-                    Mapas
-                </NavLink>
-            </li>
-            <li
-                onClick={toggle}
-                className={`text-center ${
-                    currentLocation === '/agregar' ? 'bg-primary-darker' : ''
-                } h-10 flex justify-center items-center cursor-pointer`}
-            >
-                <NavLink to="/agregar" className="text-text-color w-full">
-                    Agregar nade
-                </NavLink>
-            </li>
-        </ul>
-    )
+  const currentLocation = useLocation().pathname
+  return (
+    <ul className="fixed right-0 left-0 top-[7.5%] bg-background-start w-full shadow-custom">
+      <li
+        onClick={toggle}
+        className={`text-center ${
+          currentLocation === '/' ? 'bg-primary-darker' : ''
+        } h-10 flex justify-center items-center cursor-pointer`}
+      >
+        <NavLink to="/" className="text-text-color w-full">
+          Inicio
+        </NavLink>
+      </li>
+      <li
+        onClick={toggle}
+        className={`text-center ${
+          currentLocation === '/mapas' ? 'bg-primary-darker' : ''
+        } h-10 flex justify-center items-center cursor-pointer`}
+      >
+        <NavLink to="/mapas" className="text-text-color w-full">
+          Mapas
+        </NavLink>
+      </li>
+      <li
+        onClick={toggle}
+        className={`text-center ${
+          currentLocation === '/agregar' ? 'bg-primary-darker' : ''
+        } h-10 flex justify-center items-center cursor-pointer`}
+      >
+        <NavLink to="/agregar" className="text-text-color w-full">
+          Agregar nade
+        </NavLink>
+      </li>
+    </ul>
+  )
 }
 
 export default MobileMenuItems
--- src/pages/AddNade.tsx
+++ src/pages/AddNade.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { FcGoogle } from 'react-icons/fc'
+import useSignWithGoogle from '../hooks/useSignWithGoogle'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../firebase'
+import { ClipLoader } from 'react-spinners'
 
 const AddNade: React.FC = () => {
+    const [user, loading, error] = useAuthState(auth)
+    console.log(`The user: ${user}`)
+    console.log(`Auth state is being loaded?: ${loading}`)
+    console.log(`Error?: ${error}`)
     return (
         <div>
-            <h1 className="text-text-color drop-shadow-heading text-2xl">
-                Todavía no está implementada esa funcionalidad. <br />
-                <br />{' '}
-                <NavLink
-                    to="/"
-                    className="text-lg text-red-500 hover:underline"
-                >
-                    Volver al inicio.
-                </NavLink>
-            </h1>
+            {loading === true ? (
+                <ClipLoader />
+            ) : user !== null ? (
+                <p className="text-text-color">
+                    Ya estás iniciado sesión,{' '}
+                    <span className="text-red-500">{user?.email}</span>
+                </p>
+            ) : (
+                <div className="flex flex-col">
+                    <h1 className="text-text-color text-2xl font-medium">
+                        Antes de agregar una nade debes iniciar sesión.
+                    </h1>
+                    <h2 className="text-text-color text-md mb-4">
+                        Utiliza alguno de los siguientes métodos:
+                    </h2>
+                    <button
+                        onClick={useSignWithGoogle}
+                        className="flex items-center justify-center gap-2 bg-white p-2 font-medium w-28"
+                    >
+                        Google <FcGoogle />
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
