d3c5da5612da9a9f2dad82fc9e076d0efab5af1e
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-28 20:47:56 -0300

    Add private route /agregar/form for AddNadeForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,94 @@
 import React, { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
-import AddNade from './pages/AddNade'
+import { auth } from './firebase'
 import ThemeContextProvider from './providers/ThemeContext'
 const Homepage = lazy(() => import('./pages/Homepage'))
+const AddNade = lazy(() => import('./pages/AddNade'))
+const AddNadeForm = lazy(() => import('./pages/AddNadeForm'))
 const Mapas = lazy(() => import('./pages/Mapas'))
 const Locations = lazy(() => import('./pages/Locations'))
 const Nades = lazy(() => import('./pages/Nades'))
 
 const App: React.FC = () => {
-    const routes = [
-        {
-            path: '/',
-            element: (
-                <Suspense fallback={false}>
-                    <Homepage />
-                </Suspense>
-            )
-        },
-        {
-            path: '/mapas',
-            element: (
-                <Suspense fallback={null}>
-                    <Mapas />
-                </Suspense>
-            )
-        },
-        {
-            path: '/agregar',
-            element: (
-                <Suspense fallback={null}>
-                    <AddNade />
-                </Suspense>
-            )
-        },
-        {
-            path: '/:mapa/locations',
-            element: (
-                <Suspense fallback={null}>
-                    <Locations />
-                </Suspense>
-            )
-        },
-        {
-            path: '/:mapa/locations/:location',
-            element: (
-                <Suspense fallback={null}>
-                    <Nades />
-                </Suspense>
-            )
-        }
-    ]
-    return (
-        <ThemeContextProvider>
-            <div className="min-h-screen flex bg-gradient-to-br from-background-start to-background-end">
-                <Layout>
-                    <Routes>
-                        {routes.map((route) => (
-                            <Route
-                                key={route.path}
-                                path={route.path}
-                                element={route.element}
-                            />
-                        ))}
-                    </Routes>
-                </Layout>
-            </div>
-        </ThemeContextProvider>
-    )
+  const [user, loading] = useAuthState(auth)
+  function RequireAuth({ children }: { children: JSX.Element }) {
+    if (loading === false && user === null) {
+      return <Navigate to="/" />
+    }
+    return children
+  }
+
+  const routes = [
+    {
+      path: '/',
+      element: (
+        <Suspense fallback={false}>
+          <Homepage />
+        </Suspense>
+      )
+    },
+    {
+      path: '/mapas',
+      element: (
+        <Suspense fallback={null}>
+          <Mapas />
+        </Suspense>
+      )
+    },
+    {
+      path: '/agregar',
+      element: (
+        <Suspense fallback={null}>
+          <AddNade />
+        </Suspense>
+      )
+    },
+    {
+      path: '/agregar/form',
+      element: (
+        <Suspense fallback={null}>
+          <RequireAuth>
+            <AddNadeForm />
+          </RequireAuth>
+        </Suspense>
+      )
+    },
+    {
+      path: '/:mapa/locations',
+      element: (
+        <Suspense fallback={null}>
+          <Locations />
+        </Suspense>
+      )
+    },
+    {
+      path: '/:mapa/locations/:location',
+      element: (
+        <Suspense fallback={null}>
+          <Nades />
+        </Suspense>
+      )
+    }
+  ]
+  return (
+    <ThemeContextProvider>
+      <div className="min-h-screen flex bg-gradient-to-br from-background-start to-background-end">
+        <Layout>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+        </Layout>
+      </div>
+    </ThemeContextProvider>
+  )
 }
 
 export default App
