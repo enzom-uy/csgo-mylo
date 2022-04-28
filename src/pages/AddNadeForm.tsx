cf88dcbf8b14551521427c084c5a2c93f1a53f3e
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-28 20:47:37 -0300

    Create AddNadeForm component

---
+++ src/pages/AddNadeForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddNadeForm: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-text-color">Hola estás en el formulario</h1>
+    </div>
+  )
+}
+
+export default AddNadeForm
