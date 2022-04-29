626f81cb206d1df3bd481ce7c53f30e7c994d4d1
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-29 19:28:34 -0300

    Maybe is the ClipLoader?

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,5 +1,6 @@
-import React, { lazy } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
+import { SyncLoader } from 'react-spinners'
 const Dust2 = lazy(() => import('../components/MapLocations/Dust2'))
 const Mirage = lazy(() => import('../components/MapLocations/Mirage'))
 const Inferno = lazy(() => import('../components/MapLocations/Inferno'))
@@ -11,11 +12,31 @@ const Locations: React.FC = () => {
   const { mapa } = useParams()
   return (
     <div className="flex flex-col gap-4">
-      {mapa === 'Mirage' ? <Mirage location={location} /> : null}
-      {mapa === 'Dust 2' ? <Dust2 location={location} /> : null}
-      {mapa === 'Nuke' ? <Nuke location={location} /> : null}
-      {mapa === 'Overpass' ? <Overpass location={location} /> : null}
-      {mapa === 'Inferno' ? <Inferno location={location} /> : null}
+      {mapa === 'Mirage' ? (
+        <Suspense fallback={<SyncLoader color="#406e8e" />}>
+          <Mirage location={location} />
+        </Suspense>
+      ) : null}
+      {mapa === 'Dust 2' ? (
+        <Suspense fallback={<SyncLoader color="#406e8e" />}>
+          <Dust2 location={location} />
+        </Suspense>
+      ) : null}
+      {mapa === 'Nuke' ? (
+        <Suspense fallback={<SyncLoader color="#406e8e" />}>
+          <Nuke location={location} />
+        </Suspense>
+      ) : null}
+      {mapa === 'Overpass' ? (
+        <Suspense fallback={<SyncLoader color="#406e8e" />}>
+          <Overpass location={location} />
+        </Suspense>
+      ) : null}
+      {mapa === 'Inferno' ? (
+        <Suspense fallback={<SyncLoader color="#406e8e" />}>
+          <Inferno location={location} />
+        </Suspense>
+      ) : null}
     </div>
   )
 }
