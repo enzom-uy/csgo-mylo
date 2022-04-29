737f8ab2e234362361ff4112d618f76906cf87f8
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-29 17:21:43 -0300

    Add primary-lighter custom color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            backgroundImage: {
-                'navbar-logo': "url('/src/assets/mylo_navbarpng.png')",
-                'card-dust2': "url('/src/assets/card_dust2.png')",
-                'card-inferno': "url('/src/assets/card_inferno.png')",
-                'card-mirage': "url('/src/assets/card_mirage.png')",
-                'card-nuke': "url('/src/assets/card_nuke.png')",
-                'card-overpass': "url('/src/assets/card_overpass.png')"
-            },
-            colors: {
-                'background-start': '#161925',
-                'primary-darker': '#11131c',
-                'background-end': '#191C2E',
-                'text-color': '#406E8E'
-            },
-            boxShadow: {
-                custom: '0px 20px 17px rgba(0, 0, 0, 0.10)'
-            },
-            dropShadow: {
-                heading: '0px 4px 17px rgba(0, 0, 0, 0.43)',
-                cardInset: 'inset 255px 4px 4px rgba(0, 0, 0, 0.46)'
-            }
-        }
-    },
-    plugins: []
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'navbar-logo': "url('/src/assets/mylo_navbarpng.png')",
+        'card-dust2': "url('/src/assets/card_dust2.png')",
+        'card-inferno': "url('/src/assets/card_inferno.png')",
+        'card-mirage': "url('/src/assets/card_mirage.png')",
+        'card-nuke': "url('/src/assets/card_nuke.png')",
+        'card-overpass': "url('/src/assets/card_overpass.png')"
+      },
+      colors: {
+        'background-start': '#161925',
+        'primary-darker': '#11131c',
+        'background-end': '#191C2E',
+        'text-color': '#406E8E',
+        'primary-lighter': '#63a2cf'
+      },
+      boxShadow: {
+        custom: '0px 20px 17px rgba(0, 0, 0, 0.10)'
+      },
+      dropShadow: {
+        heading: '0px 4px 17px rgba(0, 0, 0, 0.43)',
+        cardInset: 'inset 255px 4px 4px rgba(0, 0, 0, 0.46)'
+      }
+    }
+  },
+  plugins: []
 }
