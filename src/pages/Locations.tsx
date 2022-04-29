e402d35f03f3328b1143ea8c4abe354864c29df3
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-29 19:11:04 -0300

    Maybe?

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -10,33 +10,14 @@ const Nuke = lazy(() => import('../components/MapLocations/Nuke'))
 const Locations: React.FC = () => {
   const location = useLocation().pathname
   const { mapa } = useParams()
+  console.log(mapa)
   return (
     <div className="flex flex-col gap-4">
-      {mapa === 'Mirage' ? (
-        <Suspense fallback={<ClipLoader color="#406E8E" />}>
-          <Mirage location={location} />
-        </Suspense>
-      ) : null}
-      {mapa === 'Dust 2' ? (
-        <Suspense fallback={<ClipLoader color="#406E8E" />}>
-          <Dust2 location={location} />
-        </Suspense>
-      ) : null}
-      {mapa === 'Nuke' ? (
-        <Suspense fallback={<ClipLoader color="#406E8E" />}>
-          <Nuke location={location} />
-        </Suspense>
-      ) : null}
-      {mapa === 'Overpass' ? (
-        <Suspense fallback={<ClipLoader color="#406E8E" />}>
-          <Overpass location={location} />
-        </Suspense>
-      ) : null}
-      {mapa === 'Inferno' ? (
-        <Suspense fallback={<ClipLoader color="#406E8E" />}>
-          <Inferno location={location} />
-        </Suspense>
-      ) : null}
+      {mapa === 'Mirage' ? <Mirage location={location} /> : null}
+      {mapa === 'Dust 2' ? <Dust2 location={location} /> : null}
+      {mapa === 'Nuke' ? <Nuke location={location} /> : null}
+      {mapa === 'Overpass' ? <Overpass location={location} /> : null}
+      {mapa === 'Inferno' ? <Inferno location={location} /> : null}
     </div>
   )
 }
