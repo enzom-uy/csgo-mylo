88f4617408f29d7d247a5c11d796654febd71f06
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-29 04:07:51 -0300

    Locations and nadeTypes

---
+++ src/data/locations.ts
@@ -0,0 +1,63 @@
+export const locations = [
+  {
+    map: 'Mirage',
+    locations: [
+      'site-a',
+      'liga',
+      'site-b',
+      'medio',
+      'ratas',
+      'varanda',
+      'palacio',
+      'ct',
+      'tt',
+      'tapete',
+      'mercado'
+    ]
+  },
+  {
+    map: 'Inferno',
+    locations: [
+      'site-a',
+      'corta',
+      'site-b',
+      'tapete-ct',
+      'tapete-tt',
+      'medio',
+      'medio-falso',
+      'banana'
+    ]
+  },
+  {
+    map: 'Nuke',
+    locations: ['site-a', 'site-b', 'rampa', 'silo', 'afuera', 'lobby']
+  },
+  {
+    map: 'Overpass',
+    locations: [
+      'site-a',
+      'site-b',
+      'corta',
+      'medio',
+      'fondo',
+      'banios',
+      'liga',
+      'aguas',
+      'cielo'
+    ]
+  },
+  {
+    map: 'Dust 2',
+    locations: [
+      'site-a',
+      'site-b',
+      'medio',
+      'medio-ct',
+      'fondo',
+      'varanda',
+      'oscuro',
+      'tt',
+      'ct'
+    ]
+  }
+]
---
+++ src/data/nadeTypes.ts
@@ -0,0 +1,1 @@
+export const nadeTypes = ['smoke', 'flash', 'molo']
