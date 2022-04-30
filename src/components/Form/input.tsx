cddf6e2bb1e1dad88556ea59411e9f6269ac362e
Enzo Muñoz <enzom.dev@gmail.com>
2022-04-30 14:18:05 -0300

    Fix font color issue when in dark mode

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -28,7 +28,7 @@ const Input: React.FC<Props> = ({
         value={value}
         autoComplete="off"
         className="block py-2.5 px-0 w-full text-md font-medium text-black bg-transparent
-        border-0 border-b-2 border-gray-300 appearance-none dark:text-white
+        border-0 border-b-2 border-gray-300 appearance-none dark:text-black
         dark:border-gray-600 dark:focus:border-blue-500
         focus:outline-none focus:ring-0 focus:border-blue-600 peer"
       />
@@ -38,7 +38,7 @@ const Input: React.FC<Props> = ({
         text-sm text-black dark:text-gray-400 duration-300
         transform -translate-y-6 scale-75 top-3 -z-10
         origin-[0] peer-focus:left-0 peer-focus:text-blue-600
-        peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100
+        peer-focus:dark:text-black peer-placeholder-shown:scale-100
         peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
       >
         {labelText}
